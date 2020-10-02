e4b29b2d4aab680071701bf4f3dd1f5320bf6473
TimothyGeissler <timothy.geissler@duke.edu>
2020-10-01 21:18:43 -0400

    v1.0 - finished w/ comments

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -1,11 +1,16 @@
 public class LinkStrand implements IDnaStrand{
 
     private Node myFirst, myLast;
-
     private long mySize;
-
     private int myAppends;
+    //For char at
+    private int myIndex;
+    private int myLocalIndex;
+    private Node myCurrent;
 
+    /**
+     * The Node objects that make up a LinkStrand
+     */
     private class Node {
         String info;
         Node next;
@@ -16,6 +21,10 @@ public class LinkStrand implements IDnaStrand{
         }
     }
 
+    /**
+     * Create LinkStrand representing s
+     * @param s cgat data the LinkStrand will contain
+     */
     public LinkStrand(String s) {
         initialize(s);
     }
@@ -24,17 +33,29 @@ public class LinkStrand implements IDnaStrand{
         this("");
     }
 
+    /**
+     *
+     * @return number of base pairs in LinkStrand
+     */
     @Override
     public long size() {
         return mySize;
     }
 
+    /**
+     * Initialize the LinkStrand to contain the same data as source. Also sets instance variables
+     * to the correct starting values.
+     * @param source the source of the enzyme
+     */
     @Override
     public void initialize(String source) {
         this.myFirst = new Node(source);
         this.myLast = myFirst;
         this.mySize = source.length();
         this.myAppends = 0;
+        this.myCurrent = myFirst;
+        this.myIndex = 0;
+        this.myLocalIndex = 0;
     }
 
     @Override
@@ -42,6 +63,11 @@ public class LinkStrand implements IDnaStrand{
         return new LinkStrand(source);
     }
 
+    /**
+     * Append a new strand of DNA to the LinkStrand
+     * @param dna is the string appended to this strand
+     * @return this modified LinkStrand object
+     */
     @Override
     public IDnaStrand append(String dna) {
         this.mySize = mySize + dna.length();
@@ -52,6 +78,10 @@ public class LinkStrand implements IDnaStrand{
         return this;
     }
 
+    /**
+     * Reverse the nodes of the LinkStrand as well as the String contents of the nodes
+     * @return new reversed LinkStrand
+     */
     @Override
     public IDnaStrand reverse() {
         Node i = this.myFirst;
@@ -59,40 +89,62 @@ public class LinkStrand implements IDnaStrand{
         StringBuilder sb = new StringBuilder(toRev);
         StringBuilder sbRev = sb.reverse();
         LinkStrand res = new LinkStrand(sbRev.toString());
-        //Node firstRes = res;
         i = i.next;
         while (i != null) {
             toRev = i.info;
             sb = new StringBuilder(toRev);
             sbRev = sb.reverse();
             Node temp = new Node(sbRev.toString());
-            //System.out.println("Temp: " + temp.info + " - > res: " + res.info);
             temp.next = res.myFirst;
             res.myFirst = temp;
             res.mySize = res.mySize + temp.info.length();
-            //res.next = new Node(i.info);
             i = i.next;
-            //es = res.next;
         }
         return res;
     }
 
+    /**
+     *
+     * @return number of appends called
+     */
     @Override
     public int getAppendCount() {
         return this.myAppends;
     }
 
+    /**
+     * Gets char at given index, also saves state with instance variables - if index given in next
+     * call > previous index then will scan from previous index rather than from 0.
+     * @param index specifies which character will be returned
+     * @return character at given index of LinkStrand
+     */
     @Override
     public char charAt(int index) {
-        return 0;
+        if (index < this.myIndex) {
+            this.myIndex = 0;
+            this.myLocalIndex = 0;
+            this.myCurrent = myFirst;
+        }
+        while (this.myIndex != index) {
+            this.myIndex++;
+            this.myLocalIndex++;
+            if (this.myLocalIndex >= this.myCurrent.info.length()) {
+                this.myLocalIndex = 0;
+                this.myCurrent = this.myCurrent.next;
+            }
+        }
+        return this.myCurrent.info.charAt(this.myLocalIndex);
     }
 
+    /**
+     * Combines all characters of all nodes in LinkStrand w/ StringBuilder
+     * @return String of contents of LinkStrand object
+     */
     @Override
     public String toString() {
         Node index = this.myFirst;
         StringBuilder sb = new StringBuilder();
         while (index != null) {
-            System.out.println("INFO: " + index.info);
             sb.append(index.info);
             index = index.next;
         }
